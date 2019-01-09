fb41b70b644b4f7efd27e59086babc5d55dc58d4
trymycode <imitasingha1994@gmail.com>
2019-01-09 23:49:57 +0530

    added edit, delete, goback button functions

--- src/app/blog-edit/blog-edit.component.ts
+++ src/app/blog-edit/blog-edit.component.ts
@@ -1,15 +1,57 @@
+import { catchError } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { BlogService } from '../blog.service';
+import { BlogHttpService } from './../blog-http.service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-blog-edit',
   templateUrl: './blog-edit.component.html',
-  styleUrls: ['./blog-edit.component.css']
+  styleUrls: ['./blog-edit.component.css'],
+  providers:[Location]
 })
 export class BlogEditComponent implements OnInit {
+  public currentBlog;
+  public possibleCategories = ["Comedy","Drama","Action","Technology"];
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute, private router: Router,
+    public blogService: BlogService, public blogHttpService: BlogHttpService, 
+    private location: Location) { }
 
   ngOnInit() {
+    let myBlogId = this._route.snapshot.paramMap.get('blogId');
+    console.log(myBlogId);
+    this.blogHttpService.getSingleBlogInformation(myBlogId).subscribe(
+      data =>{
+        console.log(data);
+        this.currentBlog= data["data"];
+        console.log("Current blog is:");
+        console.log(this.currentBlog);
+      },
+      error =>{
+        console.log("Some error occured");
+        console.log("Error!"+ error);
+      }
+    )
   }
-
+  public editThisBlog():any{
+    this.blogHttpService.editBlog(this.currentBlog.blogId, this.currentBlog)
+    .subscribe(
+      data =>{
+        console.log(data);
+        alert("Blog edited successfully!");
+        setTimeout(()=>{
+          this.router.navigate(['/blog',this.currentBlog.blogId]);
+        },1000);
+      },
+      error =>{
+        console.log("Some error occured");
+        console.log("Error!"+ error.message);
+        alert("Error occuered!"+ error);
+      }
+    )
+  }
+  public previousPage():any{
+    this.location.back();
+   }
 }
--- src/app/blog-view/blog-view.component.ts
+++ src/app/blog-view/blog-view.component.ts
@@ -1,16 +1,19 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BlogService } from '../blog.service';
 import { BlogHttpService } from './../blog-http.service';
+
 @Component({
   selector: 'app-blog-view',
   templateUrl: './blog-view.component.html',
-  styleUrls: ['./blog-view.component.css']
+  styleUrls: ['./blog-view.component.css'],
+  providers:[Location]
 })
 export class BlogViewComponent implements OnInit {
   public currentBlog;
   
-  constructor( private _route: ActivatedRoute, private router: Router,public blogService: BlogService, public blogHttpService: BlogHttpService) { }
+  constructor( private _route: ActivatedRoute, private router: Router,public blogService: BlogService, public blogHttpService: BlogHttpService, private location: Location) { }
 
   ngOnInit() {
     
@@ -44,5 +47,8 @@ export class BlogViewComponent implements OnInit {
       }
     )
  } 
+ public previousPage():any{
+  this.location.back();
+ }
  
 }
